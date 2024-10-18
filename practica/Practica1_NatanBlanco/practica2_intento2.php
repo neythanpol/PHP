96a183b863666855e0db6a3600dd5e8b8d28d567
neythanpol <ntn.blanco95@gmail.com>
2024-10-18 09:58:36 +0200

    practica antigua 18/10

---
+++ practica/Practica1_NatanBlanco/practica2_intento2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Natán Blanco">
+    <title>Mejor clase</title>
+</head>
+<body>
+    <?php
+    // Rellena un array llamado datos_notas de 90 elementos de manera aleatoria con valores de las notas de 3 grupos de alumnos que pertenecen a las clases A, B y C.
+    // Todas las clases tienen el mismo número de alumnos, los 30 primeros son de la clase A, etc.
+    // Calcula el número de suspensos, aprobados y medias por clase. Almacena estos datos en arrays asociativos.
+    // Calcula también los totales de aprobados y suspensos y media total.
+    // Muestra los resultados en una tabla.
+    // Muestra la mejor clase (mejor media) de color verde.
+    ?>
+</body>
+</html>
