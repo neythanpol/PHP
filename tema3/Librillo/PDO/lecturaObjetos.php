22b1b7987e0359d31719d55eea8a25715d417215
neythanpol <ntn.blanco95@gmail.com>
2024-11-19 12:09:29 +0100

    PDO 19/11

---
+++ tema3/Librillo/PDO/lecturaObjetos.php
@@ -0,0 +1,21 @@
+<?php
+    include "conexionPDO.php";
+
+    try {
+        $sql = "SELECT * FROM persona";
+
+        $sentencia = $conexion -> prepare($sql);
+        $sentencia -> setFetchMode(PDO::FETCH_OBJ);
+        $sentencia -> execute();
+
+        while ($t = $sentencia -> fetch()) {
+            echo "ID: " . $t -> id_persona . "<br />";
+            echo "Nombre: " . $t -> nombre . "<br />";
+        }
+
+        $num_filas = $sentencia -> rowCount();
+        echo "El número de filas es: " . $num_filas;
+    } catch (PDOException $e) {
+        echo $e -> getMessage();
+    }
+?>
